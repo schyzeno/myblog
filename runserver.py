29dff7d178ad1f5da06c617bc639187febbf5ab0
schyzeno <namphan00@gmail.com>
2016-07-31 21:18:25 -0500

    Initialize app and Test

---
+++ runserver.py
@@ -0,0 +1,5 @@
+from myblog import app
+app.run(debug=True,
+        host="0.0.0.0",
+        port=5000
+)
---
+++ tests/context.py
@@ -0,0 +1,5 @@
+import sys, os
+basedir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, basedir + '/../')
+
+from myblog import myblog
