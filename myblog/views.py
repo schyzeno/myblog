e0584fa288d06487194080c3abe537f0fd2a9116
schyzeno <namphan00@gmail.com>
2016-08-01 00:41:22 -0500

    add login and logout routes

--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,5 @@
 from myblog import app
-from flask import render_template, g, request, url_for, redirect, flash
+from flask import render_template, g, request, url_for, redirect, flash, session
 from myblog.database import db_session, init_db
 from myblog.models import Post, Category
 
@@ -10,3 +10,23 @@ def index():
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
